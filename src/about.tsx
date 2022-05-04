51b39c476ab683bcb05fa53738efbd3d8fb8cd3d
Anto Silviya Geo Prakash <dvsilviyageo@datavisyn.io>
2022-05-04 10:01:25 +0200

    The about component is updated to show only the table

--- src/about.tsx
+++ src/about.tsx
@@ -15,74 +15,8 @@ const About = () => {
     setSelectedDetail(detail);
   };
 
-  console.log(lineUpSelection);
-
-  const Chart = () => {
-    let x: string[] = [];
-    let y: number[] = [];
-
-    if (selDetail) {
-      const plotData: ISelDetail = {
-        maths: selDetail.maths,
-        science: selDetail.science,
-        computer: selDetail.computer,
-      };
-
-      x = Object.keys(plotData);
-
-      y = Object.values(plotData);
-      return (
-        <div className="div-wrapper mt-2">
-          <Plot
-            data={[
-              {
-                x: x,
-                y: y,
-                type: "scatter",
-                mode: "lines+markers",
-                marker: { color: "red" },
-              },
-              { type: "bar", x: x, y: y },
-            ]}
-            layout={{
-              width: 400,
-              height: 300,
-              yaxis: { title: "Marks" },
-              xaxis: { title: "Subjects" },
-              title: "visualization chart",
-            }}
-          />
-        </div>
-      );
-    } else {
-      return null;
-    }
-  };
-
-  const SelectedUserDetail = () => {
-    if (selDetail) {
-      return (
-        <div className="col md-4 mt-2 ms-3 ">
-          <div className="text-wrapper">
-            <p className="fst-italic"> Selected user:</p>
-            <p className="fw-light">
-              Username:{" "}
-              <span className="fw-lighter">{selDetail?.userName}</span>
-            </p>
-            <p className="fw-light">
-              Id: <span className="fw-lighter">{selDetail?.id}</span>
-            </p>
-            {/* <button>LineView</button> */}
-          </div>
-        </div>
-      );
-    } else {
-      return null;
-    }
-  };
-
   return (
-    <div className="container about-wrapper">
+    <div className="container">
       <div className="row">
         <div className="col md-6">
           <p>
@@ -94,7 +28,6 @@ const About = () => {
           <table className="table">
             <thead className="thead-dark">
               <tr>
-                <th scope="col">#</th>
                 <th scope="col">Name</th>
                 <th scope="col">Maths</th>
                 <th scope="col">Science</th>
@@ -105,13 +38,13 @@ const About = () => {
               {details.map((detail) => {
                 return (
                   <tr key={detail.id}>
-                    <th scope="row">
+                    {/* <th scope="row">
                       <input
                         type="radio"
                         name="radio-id"
                         onChange={() => onDetailSelected(detail)}
                       ></input>
-                    </th>
+                    </th> */}
                     <td>{detail.userName}</td>
                     <td>{detail.maths}</td>
                     <td>{detail.science}</td>
@@ -121,23 +54,6 @@ const About = () => {
               })}
             </tbody>
           </table>
-        </div>
-
-        <div className="col md-4">
-          <SelectedUserDetail />
-          <Chart />
-          {/* <LineUpComponent /> */}
-        </div>
-      </div>
-
-      <div className="row mt-5">
-        <hr />
-        <div className="mt-5">
-          <LineUpComponent
-            data={details}
-            selection={lineUpSelection}
-            onSelectionChange={setLineUpSelection}
-          />
         </div>
       </div>
     </div>
